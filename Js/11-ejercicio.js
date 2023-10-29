ebb06270d4c55a50a7f809316330bba92f0722b3
cosmeNicolas <nicomas7590@gmail.com>
2023-10-29 17:38:00 -0300

    realice la primera estructura del ejercicio 11

---
+++ Js/11-ejercicio.js
@@ -0,0 +1,17 @@
+/*11- Realiza un script 
+que pida por teclado 3 edades y 3 nombres
+ e indique el nombre del mayor. *
+
+Nota: ver funcion Math() https://www.w3schools.com/js/js_math.asp
+ */
+
+const primeraPersona = [];
+const segundaPersona = [];
+const terceraPersona = [];
+//const nombrePersona = prompt('ingrese su nombre');
+//console.log(primeraPersona[0])
+for (let nombre = 0; nombre< 3; nombre++){
+    let persona =[ prompt('ingrese su nombre')]
+    console.log(persona)
+    persona.push(primeraPersona[0])
+}

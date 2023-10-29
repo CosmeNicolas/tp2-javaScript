f5f5ba2d774ba1c2045cf451c58340832a206132
cosmeNicolas <nicomas7590@gmail.com>
2023-10-29 11:36:12 -0300

    realice el ejercicio 13 , utilizando el metodo prompt y el metodo toUpperCase() para pasar a mayusculas el texto ingresa

---
+++ Js/13-ejercicio.js
@@ -0,0 +1,8 @@
+/*Dificultad:  🟢🟡
+13- Realiza un script que pida un texto y lo muestre 
+en mayúsculas.
+*/
+
+const cadena = prompt('ingrese su texto');
+const aMayuscula = cadena.toUpperCase();
+document.write(aMayuscula);

62941c6be9bb322cb1c45d3be46f24ce1edbaf4f
CosmeNicolas <nicomas7590@gmail.com>
2023-10-30 12:29:41 -0300

    actualizo el ejercicio 11

--- Js/11-ejercicio.js
+++ Js/11-ejercicio.js
@@ -6,17 +6,23 @@ Nota: ver funcion Math() https://www.w3schools.com/js/js_math.asp
 
 
 
-for (let nombre = 0; nombre < 2; nombre++){
+for (let nombre = 0; nombre < 1; nombre++) {
     const persona = [prompt('ingrese su nombre')];
     const edad = [prompt('Ingrese su edad')];
-    const personaYedad =(persona.concat(edad));
-    
-    console.log(personaYedad);
+    const personaYedad1 = (persona.concat(edad));
+    const personaYedad2 = (persona.concat(edad));
+   // const personaYedad3 = (persona.concat(edad));
+
+    console.log(personaYedad1[0,1]);
+    console.log(personaYedad2[0,1]);
+   if(personaYedad1[0,1] > personaYedad2[0,1]){
+    alert('la primera persona es mayor a la segunda')
+    }
     //console.log(persona1);
 
-    
+
     //let personaEdad = []
-   
+
     //console.log(persona + " "+ edad);
     /*if(edad  > edad){
         console.log('La primera  es mayor a las otras 2')

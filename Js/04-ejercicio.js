c7e5d83c1febac07244e06df485c2e9fdd578103
CosmeNicolas <nicomas7590@gmail.com>
2023-10-30 22:25:01 -0300

    realice la estructura del ejercico 4

---
+++ Js/04-ejercicio.js
@@ -0,0 +1,21 @@
+/**
+ * 4- Realiza un script que pida números hasta que se pulse “cancelar”. Si no es un número deberá indicarse con un «alert» y seguir pidiendo números. Al salir con “cancelar” deberá indicarse la suma total de los números introducidos.
+ */
+
+
+const numerosSumados = [];
+
+if(confirm('desea ingresar un numero') == true){
+  for(let i = 0 ; i < parseInt(prompt('ingrese un numero')) ; i++){
+    numerosSumados.push()
+  }
+}else{
+    alert('es falso')
+  }
+  
+ 
+
+
+
+  
+

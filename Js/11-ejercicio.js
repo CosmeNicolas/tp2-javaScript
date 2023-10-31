4aa1e9c194bfa594036435ab0b740931f97e6f1f
CosmeNicolas <nicomas7590@gmail.com>
2023-10-30 21:02:56 -0300

    realice el ejercicio numero 11 pero sin utilizar el metodo math

--- Js/11-ejercicio.js
+++ Js/11-ejercicio.js
@@ -6,29 +6,65 @@ Nota: ver funcion Math() https://www.w3schools.com/js/js_math.asp
 
 
 
-for (let nombre = 0; nombre < 1; nombre++) {
+
+const personaNombre1 = prompt('ingrese su nombre');
+const personaEdad1 = parseInt(prompt('ingrese su edad'));
+
+const personaNombre2 = prompt('ingrese su nombre');
+const personaEdad2 = parseInt(prompt('ingrese su edad'));
+
+const personaNombre3 = prompt('ingrese su nombre');
+const personaEdad3 = parseInt(prompt('ingrese su edad'));
+
+
+
+
+if(personaEdad1 > personaEdad2 && personaEdad1 > personaEdad3){
+    document.write(`El usuario ${personaNombre1} de ${personaEdad1} años de edad es mayor que ${personaNombre2} y que ${personaNombre3}`)
+} else if (personaEdad2 > personaEdad1 && personaEdad2 > personaEdad3) {
+    document.write(`El usuario ${personaNombre2} de ${personaEdad2} años de edad es mayor que ${personaNombre1} y que ${personaNombre3}`)
+} else if (personaEdad3 > personaEdad1 && personaEdad3 > personaEdad2){
+    document.write(`El usuario ${personaNombre3} de ${personaEdad3} años de edad es mayor que ${personaNombre1} y que ${personaNombre2}`)
+} else {
+    alert('uno de estos usuario tiene la misma edad que los otros dos o todos tienen la misma edad')
+}
+
+
+
+
+
+
+
+//solucion con errores 
+/*for (let nombre = 0; nombre < 1; nombre++) {
     const persona = [prompt('ingrese su nombre')];
     const edad = [prompt('Ingrese su edad')];
-    const personaYedad1 = (persona.concat(edad));
-    const personaYedad2 = (persona.concat(edad));
-   // const personaYedad3 = (persona.concat(edad));
+    const personaYedad = (persona.concat(edad));
+ 
+    const persona1 = []
+    console.log(personaYedad[personaYedad.length - 1]);
 
-    console.log(personaYedad1[0,1]);
-    console.log(personaYedad2[0,1]);
-   if(personaYedad1[0,1] > personaYedad2[0,1]){
+
+
+agregar elementos a un arrelglo
+    persona1.push(personaYedad[1])
+    console.log(persona1)
+
+console.log(personaYedad2[0,1]);
+if(personaYedad1[0,1] > personaYedad2[0,1]){
     alert('la primera persona es mayor a la segunda')
-    }
-    //console.log(persona1);
+    
+console.log(persona1);
 
 
-    //let personaEdad = []
+let personaEdad = []
 
-    //console.log(persona + " "+ edad);
-    /*if(edad  > edad){
+console.log(persona + " "+ edad);
+if(edad  > edad){
         console.log('La primera  es mayor a las otras 2')
     }if(edad > edad ){
          console.log('La primera  es mayor a la 1 y 3')
     }if(edad  > edad ){
          console.log('La primera  es mayor a la 1 y 2')
-        }*/
-}
+        
+}*/

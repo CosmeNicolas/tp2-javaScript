ffb1a0c6b156b04765597919088fb62879feb7db
CosmeNicolas <nicomas7590@gmail.com>
2023-11-02 10:54:53 -0300

    realice el ejercicio 6 con dos ciclos for y un salto de linea <br> fuera del segundo ciclo pero dentro del primero , ya que deseo q el salto se haga una sol vez por cada vuelta en con cada numero

--- Js/06-ejercicio.js
+++ Js/06-ejercicio.js
@@ -13,13 +13,14 @@
 
 for(let piramide = 1 ; piramide <= 30; piramide++){
   for (let i = 1; i <= piramide; i++){
-    document.write(`<div>`)
+    
     document.write(`<strong>${piramide}</strong>`)
-    document.write(`</div>`)
+    
     
     
     
   }
+  document.write(`<br>`)
 }
 
 
